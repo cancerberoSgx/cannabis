efb3f646f1f975bdbe1e4ab1032f2fe0b5ec6b96
Sebastián Gurín <sebastigurin@gmail.com>
2019-10-05 22:32:12 -0300

    update deps

--- cannabis/src/query/getDiagnosticMessages.ts
+++ cannabis/src/query/getDiagnosticMessages.ts
@@ -1,21 +1,7 @@
-import { tsMorph } from 'ts-simple-ast-extra'
+import { tsMorph, getDiagnosticMessage } from 'ts-simple-ast-extra'
 
 export function getDiagnosticMessages(project: tsMorph.Project) {
   return project
     .getPreEmitDiagnostics()
     .map(d => getDiagnosticMessage(d))
-}
-
-function getDiagnosticMessage(d: tsMorph.Diagnostic) {
-  const s = d.getMessageText()
-  return `${d.getSourceFile() && d.getSourceFile()!.getBaseName()}: ${typeof s === 'string' ? s : print(s.getNext())}`
-}
-
-function print(s: tsMorph.DiagnosticMessageChain | undefined): string {
-  if (!s) {
-    return ''
-  }
-  else {
-    return `${s.getMessageText()} - ${print(s.getNext())}`
-  }
 }
